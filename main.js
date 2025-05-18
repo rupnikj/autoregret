e2a3148d475ced41bbb094566649fc2c20e2cf4f
rupnikj <rupnikj@users.noreply.github.com>
2025-05-18 22:38:06 +0000

    deploy: 94eec8ab94323713a451cb8d8a959bc90866add2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Entry point for AutoRegret
-const VERSION = '1747607629395';
+const VERSION = '1747607886211';
 window.VERSION = VERSION;
 import { initPanel } from './framework/ui/panel.js';
 import { loadUserApp } from './framework/core/appLoader.js';
